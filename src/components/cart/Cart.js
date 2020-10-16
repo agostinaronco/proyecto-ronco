6f63fb010edb91da7a50f4fd8becd323a4d6473d
agostina ronco <agostina.ronco@ellecktra.com>
2020-10-16 00:45:58 -0300

    modal log id compra

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,6 +3,8 @@ import { CartContext } from "./../../context/cartContext";
 import CartList from "../cart/CartList";
 import EmptyList from "../cart/EmptyList";
 import { getFirestore } from "./../../firebase";
+import {  Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 const Cart = () => {
   const [cart] = useContext(CartContext);
@@ -12,8 +14,11 @@ const Cart = () => {
     email: "",
     phone: "",
   });
-const [orderItems, setOrderItems] = useState([]);
+  const [orderItems, setOrderItems] = useState([]);
+  const [nOrder, setnOrder] = useState(null);
 
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
 
   useEffect(() => {
     var totalCart = 0;
@@ -22,36 +27,37 @@ const [orderItems, setOrderItems] = useState([]);
     });
     setTotal(totalCart);
 
-    setOrderItems(cart.map((data) => ({
-      id: data.id,
-      title: data.title,
-      price: (data.precio * data.count),
-      qty: data.count,
-    }))) ;
-
+    setOrderItems(
+      cart.map((data) => ({
+        id: data.id,
+        title: data.title,
+        price: data.precio * data.count,
+        qty: data.count,
+      }))
+    );
   }, [cart]);
 
-    const db = getFirestore();
-    const orders = db.collection("orders");
-    const newOrder = {
-      buyer: buyer,
-      items: orderItems,
-      total: total,
-    };
- 
-    const updateBuyer = (key, value) => {
-      const buyerUpdated = { ...buyer };
-      buyerUpdated[key] = value;
-      setBuyer(buyerUpdated);
-    };
-    console.log("newOrder", newOrder);
+  const db = getFirestore();
+  const orders = db.collection("orders");
+  const newOrder = {
+    buyer: buyer,
+    items: orderItems,
+    total: total,
+  };
 
-    
+  const updateBuyer = (key, value) => {
+    const buyerUpdated = { ...buyer };
+    buyerUpdated[key] = value;
+    setBuyer(buyerUpdated);
+  };
+
+
   const saveOrder = () => {
     orders
       .add(newOrder)
       .then(({ id }) => {
-        console.log(id);
+        setnOrder(id);
+        setModal(!modal);
       })
       .catch((err) => {
         console.log(err);
@@ -60,7 +66,6 @@ const [orderItems, setOrderItems] = useState([]);
         console.log("listo");
       });
   };
-
 
   return (
     <div className="row">
@@ -79,6 +84,23 @@ const [orderItems, setOrderItems] = useState([]);
           )
           //otra forma de escribirlo : cart && cart.length
         }
+      </div>
+      <div>
+        <Modal isOpen={modal}>
+          <ModalHeader>¡Generaste una nueva orden!</ModalHeader>
+          <ModalBody>
+            <p>¡Gracias por confiar en nuestro e-commerce!</p>
+            <p>Éste es tu número de orden de compra:</p>
+            <h5>{nOrder}</h5>
+            <br />
+            <p>Cualquier duda contactanos a e-commerce@me.com.ar.</p>
+          </ModalBody>
+          <ModalFooter>
+            <NavLink to="/" className="button">
+              Ir a la home
+            </NavLink>
+          </ModalFooter>
+        </Modal>
       </div>
     </div>
   );
