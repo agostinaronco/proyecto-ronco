6d247c5b388a295b534da2097d08130d87c0734a
agostina ronco <agostina.ronco@ellecktra.com>
2020-10-17 05:19:07 -0300

    remove local storage

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -5,17 +5,6 @@ export const CartContext = React.createContext();
 export const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
 
-  /*
-  useEffect(() => {
-    var myCart = localStorage.getItem("myCart");
-    setCart(myCart);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("myCart", cart);
-  }, [cart]);
-*/
-
   return (
     <CartContext.Provider value={[cart, setCart]}>
       {props.children}
